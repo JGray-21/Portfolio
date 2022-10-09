a1d21e5b89d5ebdaf841893ec3c6f5567dbc83c3
Joshua Gray <98joshua.gray17@gmail.com>
2022-10-08 23:07:21 -0500

    Further testing history function

--- my_calc.py
+++ my_calc.py
@@ -1,51 +1,53 @@
 #This is my calculator :)
 
 
-f = open("history.py", "w")
-g = open("history.py", "r")
+g = open("history.txt", "r")
+
 def add(num1, num2):
-	if num1 == 'ans': 
-		num1 = g.read()
-		print("num1 = " + num1)
-	if num2 == 'ans': num2 = g.read()
-	#f.write(str(float(num1) + float(num2)))
-	return float(num1) + float(num2)
+    if num1.rstrip() == 'ans':
+        num1 = g.read()
+    if num2.rstrip() == 'ans':
+        num2 = g.read()
+
+    f = open("history.txt", "w")
+    f.write(str(float(num1) + float(num2)))
+    f.close()
+    return float(num1) + float(num2)
 
 def sub(num1, num2):
-	f.write(str(float(num1) - float(num2)))
-	return float(num1) - float(num2)
+    f.write(str(float(num1) - float(num2)))
+    return float(num1) - float(num2)
 
 def mul(num1, num2):
-	f.write(str(float(num1) * float(num2)))
-	return float(num1) * float(num2)
+    f.write(str(float(num1) * float(num2)))
+    return float(num1) * float(num2)
 
 def div(num1, num2):
-	f.write(str(float(num1) / float(num2)))
-	return float(num1) / float(num2)
+    f.write(str(float(num1) / float(num2)))
+    return float(num1) / float(num2)
 
 while True:
-	math = input('>  ')
+    math = input('>  ')
 
-	if math.find('+') >= 0:
-		ans = add(math[:math.find('+')], math[math.find('+')+1:])
-		print(ans)
+    if math.find('+') >= 0:
+        ans = add(math[:math.find('+')], math[math.find('+')+1:])
+        print(ans)
 
-	elif math.find('-') >= 0:
-		ans = sub(math[:math.find('-')], math[math.find('-')+1:])
-		print(ans)
+    elif math.find('-') >= 0:
+        ans = sub(math[:math.find('-')], math[math.find('-')+1:])
+        print(ans)
 
-	elif math.find('*') >= 0:
-		ans = mul(math[:math.find('*')], math[math.find('*')+1:])
-		print(ans)
+    elif math.find('*') >= 0:
+        ans = mul(math[:math.find('*')], math[math.find('*')+1:])
+        print(ans)
 
-	elif math.find('/') >= 0:
-		ans = div(math[:math.find('/')], math[math.find('/')+1:])
-		print(ans)
+    elif math.find('/') >= 0:
+        ans = div(math[:math.find('/')], math[math.find('/')+1:])
+        print(ans)
 
-	elif math.find('exit') >= 0:
-		break
+    elif math.find('exit') >= 0:
+        break
 
-	else: print('Please enter valid opperation.')
+    else: print('Please enter valid opperation.')
 
-f.close()
-g.cloes()
+g.close()
