aa31d431ab5ba8224642ca210311e488db52c8ce
Joshua Gray <jgray51@vols.utk.edu>
2022-10-08 22:04:08 -0500

    Updating my_calc

--- my_calc.py
+++ my_calc.py
@@ -1,13 +1,27 @@
 #This is my calculator :)
 
+
+f = open("history.py", "w")
+g = open("history.py", "r")
 def add(num1, num2):
-	return int(num1) + int(num2)
+	if num1 == 'ans': 
+		num1 = g.read()
+		print("num1 = " + num1)
+	if num2 == 'ans': num2 = g.read()
+	#f.write(str(float(num1) + float(num2)))
+	return float(num1) + float(num2)
+
 def sub(num1, num2):
-	return int(num1) - int(num2)
+	f.write(str(float(num1) - float(num2)))
+	return float(num1) - float(num2)
+
 def mul(num1, num2):
-	return int(num1) * int(num2)
+	f.write(str(float(num1) * float(num2)))
+	return float(num1) * float(num2)
+
 def div(num1, num2):
-	return int(num1) / int(num2)
+	f.write(str(float(num1) / float(num2)))
+	return float(num1) / float(num2)
 
 while True:
 	math = input('>')
@@ -32,3 +46,6 @@ while True:
 		break
 
 	else: print('Please enter valid opperation.')
+
+f.close()
+g.cloes()
